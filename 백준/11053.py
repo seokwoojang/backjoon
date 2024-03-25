ad85a9921049b9f43ceb7ce571eb18201d41ea61
wooseokjang <jws314046@naver.com>
2024-03-25 11:25:16 +0900

    dp - bottom - up

---
+++ 백준/11053.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int,input().split()))
+dp = [1]* (n)
+
+for i in range(1, n):
+    for j in range(i):
+        if a[i] > a[j]:
+            dp[i] = max(dp[j] + 1,dp[i])
+print(max(dp))
