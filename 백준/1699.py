1ae56313a1fe96d4152215ef36679847e9b4a382
wooseokjang <jws314046@naver.com>
2024-03-28 15:30:19 +0900

    dp - bottom - up

---
+++ 백준/1699.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [i for i in range(n+1)]
+
+for i in range(1,n+1):
+    for j in range(1, i + 1):
+        s = j * j
+        if s > i:
+            break
+        if dp[i] > dp[i - s] + 1:
+            dp[i] = dp[i - s] + 1
+print(dp[n])
